174f85cb03c0a2a48c44012168924985964ff79d
7mon <simon.boutrouille123@gmail.com>
2025-11-05 00:41:10 +0100

    Fix AirportRecord

--- technical-test-api/src/main/java/technical/test/api/record/AirportRecord.java
+++ technical-test-api/src/main/java/technical/test/api/record/AirportRecord.java
@@ -7,7 +7,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
 @Builder
-@Document(collation = "airport")
+@Document(collection = "airport")
 public class AirportRecord {
     @Id
     private String iata;
